a6c2f82efe72865ae5f2b9b7c39d5121e60bd9ab
denis rodrigues <denisrodrigues.ita@gmail.com>
2022-11-02 16:51:11 -0300

    button menu

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { NavLink, Link, useNavigate } from 'react-router-dom';
 import { BiCameraMovie } from 'react-icons/bi';
 import { BsSearch } from 'react-icons/bs';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import styles from './Header.module.css';
 
 const Header = () => {
+  const [btnActive, setBtnActive] = React.useState(false);
   const [search, setSearch] = React.useState('');
   const navigate = useNavigate();
 
@@ -19,9 +21,11 @@ const Header = () => {
     setSearch('');
   }
 
+  console.log(btnActive)
+
   return (
     <header className={styles.header}>
-      <div>
+      <div className={styles.logo}>
         <Link to="/">
           <div>
             <BiCameraMovie />
@@ -32,21 +36,24 @@ const Header = () => {
           </div>
         </Link>
       </div>
-      <nav>
-        <ul>
-          <li><NavLink to='/top-rated-movies'>Top Rated Movies</NavLink></li>
-          <li><NavLink to='/top-rated-series'>Top Rated Series</NavLink></li>
-          <li><NavLink to='/trending'>Trending</NavLink></li>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder="Search"
-              onChange={handleChange}
-              value={search} />
-            <button type="submit"><BsSearch /></button>
-          </form>
-        </ul>
-      </nav >
+      <button onClick={() => setBtnActive(!btnActive)} className={styles.btnMenu}>Menu <GiHamburgerMenu /></button>
+      <div className={styles.navigation}>
+        <nav className={`${btnActive ? styles.btnMenuActive : styles.btnMenuDisabled}`}>
+          <ul>
+            <li><NavLink to='/top-rated-movies'>Top Rated Movies</NavLink></li>
+            <li><NavLink to='/top-rated-series'>Top Rated Series</NavLink></li>
+            <li><NavLink to='/trending'>Trending</NavLink></li>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Search"
+                onChange={handleChange}
+                value={search} />
+              <button type="submit"><BsSearch /></button>
+            </form>
+          </ul>
+        </nav>
+      </div>
     </header >
   );
 }
--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -2,27 +2,40 @@ import React from 'react';
 import { useSearchParams, Link } from 'react-router-dom';
 import { apiKey, baseUrl, baseUrlImage } from '../../Api';
 import { AiFillStar } from 'react-icons/ai';
+import Pagination from '../../Components/Pagination';
 
 const Search = () => {
 
-  const [search, setSearch] = React.useState([]);
+  const [search, setSearch] = React.useState(null);
   const [searchParams] = useSearchParams();
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [lastPage, setLastPage] = React.useState(null);
   const query = searchParams.get('q');
 
   React.useEffect(() => {
     const fetchSearch = async () => {
-      const response = await fetch(`${baseUrl}/search/multi${apiKey}&query=${query}`);
+      const response = await fetch(`${baseUrl}/search/multi${apiKey}&query=${query}&page=${currentPage}`);
       const json = await response.json();
-      setSearch(json.results);
+      setSearch(json);
+      json.total_pages > 500 ? setLastPage(500) : setLastPage(json.total_pages);
     }
     fetchSearch();
-  }, [query, search]);
-    
-    if (search)
+  }, [query, search, lastPage, currentPage]);
+
+  const pageNavigation = (p) => {
+    setCurrentPage(p);
+  }
+
+  if (search)
     return (
       <section>
+        <Pagination
+          lastPage={lastPage}
+          pageNavigation={pageNavigation}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage} />
         <div className='container'>
-          {search.map(item => (
+          {search.results.map(item => (
             <div key={item.id}>
               <img src={baseUrlImage + item.poster_path} alt={item.title} />
               <p>Title: {item.title ? item.title : item.name}</p>
